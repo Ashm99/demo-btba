de5a0aa43f6b0e4da6b9d33cf51da926fe98ee39
Ashm99 <ashok.ashm99@gmail.com>
2024-10-09 07:44:05 +0530

    added spring boot project from initializr

---
+++ busticketbookingapplication/src/main/java/com/example/btba/busticketbookingapplication/BusticketbookingapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.example.btba.busticketbookingapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusticketbookingapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusticketbookingapplicationApplication.class, args);
+	}
+
+}
